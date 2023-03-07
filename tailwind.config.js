54e0e08354aff0f7e91ed6092a63c2cc8dab06ac
andohfrancis9187gmail.com <andohfrancis9187@gmail.com>
2023-03-07 15:16:45 +0000

    features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,26 +10,28 @@ module.exports = {
 
         },
         
-      colors:{
-        brightRed: 'hsl(12, 88%, 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight: 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(233, 12%, 13%)',
-        veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
-        // firstSectionColor: "#505170",
-        // secondSectionColor:"F8FAFF",
-        // underHeader:"#F4F4F4",
-        // Clickbuttons:"#008EBD",
-        // bgColor:"#FFFFFF",
-        // footer:"1E1E29",
-
-
-
+      
+      extend: {
+        colors:{
+          brightRed: 'hsl(12, 88%, 59%)',
+          brightRedLight: 'hsl(12, 88%, 69%)',
+          brightRedSupLight: 'hsl(12, 88%, 95%)',
+          darkBlue: 'hsl(228, 39%, 23%)',
+          darkGrayishBlue: 'hsl(227, 12%, 61%)',
+          veryDarkBlue: 'hsl(233, 12%, 13%)',
+          veryPaleRed: 'hsl(13, 100%, 96%)',
+          veryLightGray: 'hsl(0, 0%, 98%)',
+          // firstSectionColor: "#505170",
+          // secondSectionColor:"F8FAFF",
+          // underHeader:"#F4F4F4",
+          // Clickbuttons:"#008EBD",
+          // bgColor:"#FFFFFF",
+          // footer:"1E1E29",
+  
+  
+  
+        },
       },
-      extend: {},
     },
     plugins: [],
   }
