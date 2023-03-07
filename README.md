69a5cefd544e25513e04b09abb1086f8011d825d
andohfrancis9187gmail.com <andohfrancis9187@gmail.com>
2023-03-07 11:48:23 +0000

    starter boilplate

---
+++ README.md
@@ -0,0 +1,17 @@
+<ins>#MY FIRST LANDING PAGE WITH TAILWIND CSS</ins>
+
+<ins>#Special thanks to Mr. Oliver Mensah</ins>
+This project was inspired by  traversymedia and Js mastery who are my mentors, I learnt Tailwind because of its 
+#INTEGRATED CONTENT
+###tailwindcss
+###html
+
+
+
+
+#SOME KEY POINTS I NOTED WHEN USING TAILWINDCSS
+1. Tailwind "container" class does not automcatically center the content as in bootstrap and you may have to use mx-auto to keep it centered
+2. Always use a relative class to keep the content in position
+3. Its a better practice to have the breakpoints classes at the end of each line
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],     //configers and looks for the html for for compilation 
+    theme: {
+     screens: {
+        sm:"480px",
+        md:"768px",
+        lg:"976px",
+        xl:"1440px"
+
+        },
+        
+      colors:{
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        // firstSectionColor: "#505170",
+        // secondSectionColor:"F8FAFF",
+        // underHeader:"#F4F4F4",
+        // Clickbuttons:"#008EBD",
+        // bgColor:"#FFFFFF",
+        // footer:"1E1E29",
+
+
+
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
+
+
+
